8ab37a10e8718b695f46153fe7f8e3fc069c7691
s29862-pj <146528063+s29862-pj@users.noreply.github.com>
2023-09-30 16:47:50 +0200

    Add files via upload
    Dodaje rozwiązanie do zadania 1.4

---
+++ Ćwiczenia1.4/Ćwiczenia1.4#/Ćwiczenia1.4#.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <list>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	
+	int number1;
+	int number2;
+	int number3;
+	
+	cout << "Podaj dowolne trzy liczby lub cyfry: ";
+	cin >> number1 >> number2 >> number3;
+	list<int> numbers = { number1, number2, number3 };
+
+	cout << "Oto największa liczba/cyfra: ";
+	cout << max({number1, number2, number3}) << endl;
+
+	}
